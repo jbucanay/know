3263bd221bd896ec878f4c274121071a94fdd773
gilbert baracka <gilbert@gilberts-MacBook-Air.local>
2019-06-14 09:34:40 -0500

    make code work in proven branch then merge later

--- components/Home.js
+++ components/Home.js
@@ -1,8 +1,60 @@
 import React, { Component } from "react";
-import { Text } from "react-native";
+import { Text, StyleSheet, View } from "react-native";
+import Icon from "react-native-vector-icons/Ionicons";
 
 export default class Home extends Component {
   render() {
-    return <Text>Hit</Text>;
+    return (
+      <View style={styles.container}>
+        <View style={styles.iconCont}>
+          <View style={styles.first}>
+            <Icon name="ios-home" color="gray" size={20} />
+            <Text> Home </Text>
+          </View>
+          <View style={styles.single}>
+            <Icon name="ios-wifi" color="gray" size={20} />
+            <Text>Tech</Text>
+          </View>
+          <View style={styles.single}>
+            <Icon name="ios-contacts" color="gray" size={20} />
+            <Text>Social</Text>
+          </View>
+          <View style={styles.single}>
+            <Icon name="ios-globe" color="gray" size={20} />
+            <Text>Global</Text>
+          </View>
+          <View style={styles.single}>
+            <Icon name="ios-trending-up" color="gray" size={20} />
+            <Text>Trending</Text>
+          </View>
+        </View>
+      </View>
+    );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "flex-end",
+    backgroundColor: "#F5FCFF",
+    marginBottom: 24
+  },
+  single: {
+    alignItems: "center",
+    marginLeft: 15
+  },
+  first: {
+    alignItems: "center"
+  },
+
+  iconCont: {
+    paddingTop: 7,
+    borderTopColor: "red",
+    flexDirection: "row",
+    justifyContent: "space-evenly",
+    backgroundColor: "white",
+    alignSelf: "stretch"
+  }
+});
