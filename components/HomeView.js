d647e69beb36428118344f53bc075c1965434fbe
gilbert baracka <gilbert@gilberts-MacBook-Air.local>
2019-06-22 16:44:33 -0500

    render home page

--- components/HomeView.js
+++ components/HomeView.js
@@ -1,10 +1,29 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet, Image } from "react-native";
 
 export default class HomeView extends Component {
   render() {
-    console.log(this.props.children);
-    return <Text>Waiting..</Text>;
+    if (this.props.children.length) {
+      const one = this.props.children[0];
+      console.log(one);
+      return (
+        <View>
+          <Image source={{ uri: one.urlToImage }} style={styles.img} />
+        </View>
+      );
+    }
+    return (
+      <View>
+        <Text>Loading...</Text>
+      </View>
+    );
   }
 }
+
+const styles = StyleSheet.create({
+  img: {
+    width: 375,
+    height: 200
+  }
+});
