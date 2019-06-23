71dea95b3949774b52aca8dda16404bd3cfcc773
gilbert baracka <gilbert@gilberts-MacBook-Air.local>
2019-06-23 06:49:25 -0500

    scrollable

--- components/HomeView.js
+++ components/HomeView.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
-import { View, Text, StyleSheet, Image } from "react-native";
+import { View, Text, StyleSheet, Image, ScrollView } from "react-native";
 import AllHome from "./AllHome";
 
 /// responsibility: show home news
@@ -12,7 +12,7 @@ export default class HomeView extends Component {
       const date = new Date(one.publishedAt);
       const newsDate = date.toString().slice(0, 10);
       return (
-        <View style={styles.parent}>
+        <ScrollView style={styles.parent}>
           <View style={styles.imgCont}>
             <Image source={{ uri: one.urlToImage }} style={styles.img} />
             <Text style={styles.credit}>{one.author}</Text>
@@ -27,7 +27,7 @@ export default class HomeView extends Component {
               return <AllHome key={index} all={item} />;
             })}
           </View>
-        </View>
+        </ScrollView>
       );
     }
     return (
