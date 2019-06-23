984fd8cd850f853c34f1844ccb631414c666852a
gilbert baracka <gilbert@gilberts-MacBook-Air.local>
2019-06-23 13:28:14 -0500

    breaking

--- App.js
+++ App.js
@@ -1,17 +1,22 @@
 import React, { Component } from "react";
-import { View, Text } from "react-native";
+import { View } from "react-native";
 import Home from "./components/Home";
 import Tech from "./components/Tech";
-import { NativeRouter, Route, Link } from "react-router-native";
+// import {NativeRouter, Route, Link} from 'react-router-native'
+
+const RootStack = createStackNavigator(
+  {
+    Land: Home
+  },
+  {
+    initialRouteName: "Land"
+  }
+);
+
+const AppCont = createAppContainer(RootStack);
 
 export default class App extends Component {
   render() {
-    return (
-      <NativeRouter>
-        <Text>Hit</Text>
-        <Route exact path="/home" component={Home} />
-        <Route path="/tech" component={Tech} />
-      </NativeRouter>
-    );
+    return <AppCont />;
   }
 }
