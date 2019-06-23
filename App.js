05600fba52b03b3ce8c787bf642c99026f2e5ca8
gilbert baracka <gilbert@gilberts-MacBook-Air.local>
2019-06-23 13:48:31 -0500

    crashing

--- App.js
+++ App.js
@@ -2,21 +2,16 @@ import React, { Component } from "react";
 import { View } from "react-native";
 import Home from "./components/Home";
 import Tech from "./components/Tech";
-// import {NativeRouter, Route, Link} from 'react-router-native'
-
-const RootStack = createStackNavigator(
-  {
-    Land: Home
-  },
-  {
-    initialRouteName: "Land"
-  }
-);
-
-const AppCont = createAppContainer(RootStack);
+import { NativeRouter, Route, Link } from "react-router-native";
 
 export default class App extends Component {
   render() {
-    return <AppCont />;
+    return (
+      <NativeRouter>
+        <Text>Hit</Text>
+        <Route exact path="/" component={Home} />
+        <Route path="/tech" component={Tech} />
+      </NativeRouter>
+    );
   }
 }
