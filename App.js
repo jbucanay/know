38f89d077771e871447b9da31d333ede364af03f
gilbert baracka <gilbert@gilberts-MacBook-Air.local>
2019-06-24 11:59:41 -0500

    navigation works

--- App.js
+++ App.js
@@ -1,33 +1,4 @@
-import React, { Component } from "react";
-import Tech from "./components/Tech";
+import { Navigation } from "react-native-navigation";
 import Home from "./components/Home";
-import { View, Text } from "react-native";
-import { createStackNavigator, createAppContainer } from "react-navigation";
 
-const RootStack = createStackNavigator(
-  {
-    Home: {
-      screen: Home,
-      navigationOptions: {
-        title: "Home News"
-      }
-    },
-    Tech: {
-      screen: Tech,
-      navigationOptions: {
-        title: "Tech"
-      }
-    }
-  },
-  {
-    initialRouteName: "Home"
-  }
-);
-
-const AppCont = createAppContainer(RootStack);
-
-export default class App extends Component {
-  render() {
-    return <AppCont />;
-  }
-}
+Navigation.registerComponent("landingNews.Home", () => Home);
